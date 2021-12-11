a9dd7a4338c4d3d42650428c6053ce640e5b17fb
DUYKH0A <92005805+DUYKH0A@users.noreply.github.com>
2021-12-11 21:02:58 +0700

    Create QuanLyNV.h

---
+++ QuanLyNV.h
@@ -0,0 +1,20 @@
+#include"NhanVien.h"
+#pragma once
+class QLNV{
+    protected:
+    int _number;
+    NhanVien *_p;
+    public:
+    QLNV(int=0,NhanVien*p=nullptr);
+    ~QLNV();
+    void Save();    //lưu lại thông tin nv vào file sau khi chỉnh sửa
+    void Show();    //show thông tin nv
+    void Add(NhanVien);                   //2 hàm dùng để thêm nv
+    void Add(int,string,string,string);   
+    void Addelement();                    //them 1 luc nhieu nv
+    void Delete(int);                     //xoa 1 nv
+    void Remove();                        //xoa nhieu nv
+    NhanVien& operator[](int);            //đa năng hoá toán tử
+    void Reset();                         //reset dsnv
+    int getNumber();                      
+};
