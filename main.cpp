33c6cf35ed673f7582f02f97d028d409a7a594b3
holequotin <69898497+holequotin@users.noreply.github.com>
2021-12-13 21:07:46 +0700

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include"QuanLyPhienLamViec.h"
 #include<iostream>
 using namespace std;
+void ManHinhChinh(bool dangnhap){
+    if(dangnhap==true) QuanLy();
+    else NhanVien();
+}
+//chi ra lam
+void QuanLy(){
+    QuanLyPhienLamViec plv;
+    QuanLyHangHoa hh;
+    QuanLyNhanVien nv;
+    QuanLyBan b;
+    cout<<"Menu chuc nang"<<endl;
+    cin>>aashfk;
+    switch...case(x)
+        1:QuanLyThuChi();
+        2:QuanLyHangHoa();
+        3:QuanLyNhanVien();
+        4:BuonBan();
+}    
+void NhanVien()
+
 int main(){
     QuanLyPhienLamViec a;
-    a.DangNhap();
+    a.DangNhap();//nhả ra
 }    
