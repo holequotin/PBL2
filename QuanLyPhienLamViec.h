bb38f934aca5e66eb7e1b935db2df25073e63574
DUYKH0A <92005805+DUYKH0A@users.noreply.github.com>
2021-12-14 15:32:35 +0700

    Update QuanLyPhienLamViec.h

--- QuanLyPhienLamViec.h
+++ QuanLyPhienLamViec.h
@@ -13,10 +13,10 @@ class QuanLyPhienLamViec{
     public:
         QuanLyPhienLamViec();
         ~QuanLyPhienLamViec();
-        void DangNhap();            //đăng nhập
-        void LuuTruTienNV(int);     //lưu trữ tiền nv thu được trong 1 ngày,tham số truyền vào là gì
+        int DangNhap();            //đăng nhập
+        void LuuTruTienNV(int);     //lưu trữ tiền nv thu được trong 1 ngày
         void TongTienNV();          //hổ trợ hàm trên
         void xuatNV();              //xuất tên nv, để chọn tên khi dùng chuc năng khác
-        void xuattimedn(int);       //xuất thời gian nv đăng nhập,tham số truền vào??
-        void xuatTNNV(int);         //xuất tiền nv thu trong các ngày có time kèm,tham số truyền vào??
+        void xuattimedn(int);       //xuất thời gian nv đăng nhập
+        void xuatTNNV(int);         //xuất tiền nv thu trong các ngày có time kèm
 };
