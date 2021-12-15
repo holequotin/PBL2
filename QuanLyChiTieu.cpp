65917d8b03872c94e98b43dfb442c340f40b4331
DUYKH0A <92005805+DUYKH0A@users.noreply.github.com>
2021-12-15 16:20:07 +0700

    Update QuanLyChiTieu.cpp

--- QuanLyChiTieu.cpp
+++ QuanLyChiTieu.cpp
@@ -1,64 +1,68 @@
 #include"QuanLyChiTieu.h"
-QuanLyChiTieu::QuanLyChiTieu(int a, int b){
-    this->TongTienThu=a;
-    this->TongTienChi=b;
+QuanLyChiTieu::QuanLyChiTieu(){
 }
 QuanLyChiTieu::~QuanLyChiTieu(){}
-void QuanLyChiTieu::ThuTien(int TongTien){             //hàm dùng để lưu tổng tiền và tiền theo hoá đơn
+void QuanLyChiTieu::ThuTien(int TongTien){
     TongTienThu += TongTien;
     string str;
     int a;
     ifstream xuattien("TienThu.txt");
     if(!xuattien.eof()){
-        getline(xuattien,str);                         //đọc file đã cho sẵng, để xem tổng tiền đã thu được bao nhiêu
+        getline(xuattien,str);
         xuattien>>a;
-    }       
-    a+=TongTien;                                        //sau đó sẽ cộng thêm tiền từ hoá đơn mới thanh toán vào
+    }
+    xuattien.close();
+    a+=TongTien;
     ofstream FileThu("TienThu.txt",ios::trunc);
     FileThu<<"Tong tien da thu:"<<endl;
-    FileThu<<a;                                         //và ghi vào file
+    FileThu<<a;
     FileThu.close();
     time_t curr_time;
     curr_time = time(NULL);
-    char *tm = ctime(&curr_time);                       // ghi thời gian
+    char *tm = ctime(&curr_time);
     ofstream FileThuHoaDon("TienThuHoaDon.txt",ios::app);
-    FileThuHoaDon<<"Tong tien da thu cua hoa don vao:"<< tm ;   //tổng theo hoá đơn, sẽ ghi tiếp ko ghi đè
+    FileThuHoaDon<<"Tong tien da thu cua hoa don vao:"<< tm ;
     FileThuHoaDon<<TongTien << endl;
     FileThuHoaDon.close();
 }
-int QuanLyChiTieu::XuatTienThu(){                   //in ra tổng tiền đã thu 
+int QuanLyChiTieu::XuatTienThu(){
     string str;
     int a;
-    cout<<"Tong tien da thu:"<<endl;
+    //cout<<"Tong tien da thu:"<<endl;
     ifstream xuattien("TienThu.txt");
     if(!xuattien.eof()){
-        getline(xuattien,str);                      //đọc file và xuất
+        getline(xuattien,str);
         xuattien>>a;
     }
     cout<< a;
     return 0;
 }
-int QuanLyChiTieu::XuatTienThuHD(){                 //xuất tiền thu theo hoá đơn, chỉ có theo ngày ko rõ ràng
-
+int QuanLyChiTieu::XuatTienThuHD(){
+    string str;
+    int a;
     cout<<"Tong tien da thu theo hoa don:"<<endl;
     ifstream xuattien("TienThuHoaDon.txt");
+    // if(!xuattien.eof()){
+    //     getline(xuattien,str);
+    //     xuattien>>a;
+    // }
     string HD ;
     while (!xuattien.eof())
-    {                                                //đọc file và xuất tiền ra
+    {   
         getline(xuattien,HD);
         cout<<HD<< endl;
         HD = "";
     }
     return 0;
 }
-void QuanLyChiTieu::ChiTien(int tientieu){          //lưu tổng tiền chi vào file
+void QuanLyChiTieu::ChiTien(int tientieu){
     TongTienChi += tientieu;
     ofstream FileChi("TienChi.txt",ios::trunc);
     FileChi<<"Tong tien da chi:"<<endl;
     FileChi<<TongTienChi;
     FileChi.close();
 }
-int QuanLyChiTieu::XuatTienChi(){                   //in ra tổng tiền đã chi,khi nhập hàng hoá vào kho
+int QuanLyChiTieu::XuatTienChi(){
     string str;
     int a;
     ifstream XuatTienChi("TienChi.txt");
@@ -68,9 +72,9 @@ int QuanLyChiTieu::XuatTienChi(){                   //in ra tổng tiền đã c
     }
     return a;
 }
-void QuanLyChiTieu::resetTienThu(){                 //hàm dùng để reset file
+void QuanLyChiTieu::resetTienThu(){
     FILE *f = fopen("TienThu.txt", "w");
 }
-void QuanLyChiTieu::resetTienThuHD(){               //hàm dùng để reset file
+void QuanLyChiTieu::resetTienThuHD(){
     FILE *f = fopen("TienThuHoaDon.txt", "w");
 }
